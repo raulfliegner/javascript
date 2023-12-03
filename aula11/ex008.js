c48ee23284b903be957d1c8d6e2815c009de2d9f
raulfliegner <72817181+raulfliegner@users.noreply.github.com>
2023-12-03 20:24:07 -0300

    Aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 78
+console.log(`A velocidade do seu veículo é de ${vel}km/h.`)
+if (vel > 60)  { //condição simples.
+    console.log(`Você ultrapassou o limite de velocidade! MULTADO`)
+} else {
+    console.log('Você está no limite, dirija com cuidado!')
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Seu país é ${país}!`)
+if (país == 'Brasil') {
+    console.log(`Você é brasileiro!`)
+} else {
+    console.log(`Você é estrangeiro!`)
+}
