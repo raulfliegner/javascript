73fd1207f6e0fee4c8994dddf522e3b1c17fa560
raulfliegner <72817181+raulfliegner@users.noreply.github.com>
2023-12-05 17:46:28 -0300

    aula 12

---
+++ aula12/ex012b.js
@@ -0,0 +1,8 @@
+var rua = 'esquerda'
+if (rua == 'reto') {
+    console.log('Você seguiu em frente.')
+} else if (rua == 'direita') {
+    console.log('Você foi para a direita.')
+} else if (rua == 'esquerda') {
+    console.log('Você foi para a esquerda.')
+}
