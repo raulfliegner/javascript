cde3b91f94061e00693182d04a341847219308ea
Raul Fliegner <72817181+raulfliegner@users.noreply.github.com>
2024-05-06 05:11:11 -0300

    exercício 3 javascript

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,9 +1,49 @@
 function Calcular() {
     var data = new Date()
     var ano = data.getFullYear()
-    formano = document.getElementById('txtano')
-    anonasc = Number(formano.value)
-    if (anonasc <= 0 || anonasc > ano) {
-        window.alert(`[ERRO] Verifique as informações e tente novamente.`)
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade <= 10) {
+                //criança
+                img.setAttribute('src', 'homem-criança.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'homem-jovem.png')
+            } else if (idade <= 50) {
+                //adulto
+                img.setAttribute('src', 'homem-adulto.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'homem-velho.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade <= 10) {
+                //criança
+                img.setAttribute('src', 'mulher-criança.png')
+            } else if (idade > 10 && idade < 21) {
+                //jovem
+                img.setAttribute('src', 'mulher-jovem.png')
+            } else if (idade >= 21 && idade <= 50) {
+                //adulta
+                img.setAttribute('src', 'mulher-adulta.png')
+            } else {
+                //idosa
+                img.setAttribute('src', 'mulher-idosa.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
